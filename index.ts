4e4e8003006a825512651b6ff0ff05702c3ce84e
pchchv <ipchchv@gmail.com>
2024-01-25 12:23:21 +0300

    Implement a GET method

--- index.ts
+++ index.ts
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to the RESTful API!');
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
